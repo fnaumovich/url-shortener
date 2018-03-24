a7d0ac783c8668278b738896512cbe2cd85f0d68
Naumovich Fedor <fnaumovich@gmail.com>
2018-03-24 20:13:01 +0400

    add form handler

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const input = document.querySelector('.js-input');
-    const result = document.querySelector('.js-result');
-    const submitBtn = document.querySelector('.js-result');
+    const resultField = document.querySelector('.js-result');
+    const submitBtn = document.querySelector('.js-submit');
+
+    submitBtn.addEventListener('click', () => {
+        const inputValue = input.value;
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `/shorten?url=${inputValue}`);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (this.readyState !== 4) return;
+
+            if (this.status !== 200) {
+                alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+                return;
+            } else {
+                const result = JSON.parse(this.responseText);
+                resultField.innerHTML = result.value;
+            }
+        }
+    });
 });
