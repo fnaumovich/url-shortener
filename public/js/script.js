fc97a649c7bd71acea0d42e3a8bd02bfff755d39
Fedor Naumovich <fnaumovich@htc-cs.ru>
2018-03-26 14:13:37 +0400

    add client validation

--- public/js/script.js
+++ public/js/script.js
@@ -2,23 +2,47 @@ document.addEventListener('DOMContentLoaded', function () {
     const input = document.querySelector('.js-input');
     const resultField = document.querySelector('.js-result');
     const submitBtn = document.querySelector('.js-submit');
+    const errorMsg = document.querySelector('.js-error');
 
     submitBtn.addEventListener('click', () => {
-        const inputValue = input.value;
+        const regexpHyperlinkToHttp = /(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/ig;
+        const regexpHyperlinkHttp = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/ig;
+
+        let inputValue = input.value;
         const xhr = new XMLHttpRequest();
 
-        xhr.open('GET', `/shorten?url=${inputValue}`);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (this.readyState !== 4) return;
+        if (inputValue.match(regexpHyperlinkToHttp)) {
+            inputValue = 'http://' + inputValue;
+        }
 
-            if (this.status !== 200) {
-                alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
-                return;
-            } else {
-                const result = JSON.parse(this.responseText);
-                resultField.innerHTML = result.value;
+        if (inputValue.match(regexpHyperlinkHttp)) {
+            errorMsg.classList.remove('show');
+
+            xhr.open('GET', `/shorten?url=${inputValue}`);
+            xhr.send();
+            xhr.onreadystatechange = function() {
+                if (this.readyState !== 4) return;
+
+                if (this.status !== 200) {
+                    alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+                    return;
+                } else {
+                    const result = JSON.parse(this.responseText);
+                    resultField.innerHTML = result.value;
+                    clearField(input)
+                }
             }
+        } else {
+            errorMsg.classList.add('show');
+            clearField(resultField);
         }
     });
 });
+
+function clearField(field) {
+    if (field.value) {
+        field.value = ''
+    } else {
+        field.innerHTML = ''
+    }
+}
